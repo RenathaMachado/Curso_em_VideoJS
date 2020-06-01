21b3ff4bdbcc91bcc5612b4dd224d96caba18f3c
RenathaMachado <renatharenascer@icloud.com>
2020-06-01 11:57:22 -0300

    Aulas concluidas
    repetições

---
+++ Aula14/repeticoes-parte2.js
@@ -0,0 +1,18 @@
+
+/*var c = 1
+while(c<=10){
+  console.log(c)
+  c++
+}*/
+
+/*for(var c = 1;c<= 10; c++){
+  console.log(c)
+}
+*/
+var num1=2
+var num2=50
+var passo = 3
+while(num2>num1){
+  console.log(passo)
+  num1=passo<num2
+}
